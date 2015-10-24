92d4227166246f04e9b1508cb4a14b59db266465
Nick Grifka <ngrifka@umich.edu>
2015-10-24 19:06:50 -0400

    Added reminder item data class and category enum. More categories need to be added to the enum

---
+++ CloseBuy/app/src/main/java/fourpointoh/closebuy/Category.java
@@ -0,0 +1,8 @@
+package fourpointoh.closebuy;
+
+/**
+ * Created by nick on 24/10/15.
+ */
+public enum Category {
+    GROCERY, PHARMACY, BAKERY
+}
---
+++ CloseBuy/app/src/main/java/fourpointoh/closebuy/ReminderItem.java
@@ -0,0 +1,12 @@
+package fourpointoh.closebuy;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nick on 24/10/15.
+ */
+public class ReminderItem {
+    public int id;
+    public String itemName;
+    public ArrayList<Category> categories;
+}
