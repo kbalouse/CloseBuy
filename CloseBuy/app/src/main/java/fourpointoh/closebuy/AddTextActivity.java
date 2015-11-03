94c17fdd36d4527e2a07eaaec5bf7a3e076de7d4
Josh Tan <joshcx@umich.edu>
2015-11-03 14:00:05 -0500

    minor change

--- CloseBuy/app/src/main/java/fourpointoh/closebuy/AddTextActivity.java
+++ CloseBuy/app/src/main/java/fourpointoh/closebuy/AddTextActivity.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
-
+import android.view.inputmethod.InputMethodManager;
+import android.content.Context;
 public class AddTextActivity extends AppCompatActivity {
     String msg = "Android : ";
     private TextView switchStatus;
     private Switch mySwitch;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,14 @@ public class AddTextActivity extends AppCompatActivity {
         Log.d(msg, "The AddTextActivity onCreate() event");
         switchStatus = (TextView) findViewById(R.id.switchStatus);
         mySwitch = (Switch) findViewById(R.id.mySwitch);
-
+        editText = (EditText) findViewById(R.id.edit_message);
+        /*
+        this.post(new Runnable() {
+            public void run() {
+                editText.requestFocus();
+            }
+        });
+        */
         //set the switch to ON
         mySwitch.setChecked(false);
         //attach a listener to check for changes in state
@@ -47,6 +57,10 @@ public class AddTextActivity extends AppCompatActivity {
         else {
             switchStatus.setText("Switch is currently OFF");
         }//else
+        //editText.requestFocus();
+        //InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        //imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+
     } // onCreate
 
 } // addTextActivity
