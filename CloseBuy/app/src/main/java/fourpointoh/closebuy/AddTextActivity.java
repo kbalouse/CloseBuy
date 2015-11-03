f33b8d082f89d3264f6e68b4722ba8ec0ec54ed8
Josh Tan <joshcx@umich.edu>
2015-11-03 00:09:09 -0500

    basic layout for AddTextActivity page

--- CloseBuy/app/src/main/java/fourpointoh/closebuy/AddTextActivity.java
+++ CloseBuy/app/src/main/java/fourpointoh/closebuy/AddTextActivity.java
@@ -2,12 +2,52 @@ package fourpointoh.closebuy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class AddTextActivity extends AppCompatActivity {
+    String msg = "Android : ";
+    private TextView switchStatus;
+    private Switch mySwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_text);
-    }
-}
+        Log.d(msg, "The AddTextActivity onCreate() event");
+        switchStatus = (TextView) findViewById(R.id.switchStatus);
+        mySwitch = (Switch) findViewById(R.id.mySwitch);
+
+        //set the switch to ON
+        mySwitch.setChecked(false);
+        //attach a listener to check for changes in state
+        mySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+
+                if(isChecked){
+                    switchStatus.setText("Switch is currently ON");
+                } // if
+                else{
+                    switchStatus.setText("Switch is currently OFF");
+                } // else
+
+            } // onCheckedChanged
+        }); // setOnCheckedChangeListener
+
+        //check the current state before we display the screen
+        if(mySwitch.isChecked()){
+            switchStatus.setText("Switch is currently ON");
+        } // if
+        else {
+            switchStatus.setText("Switch is currently OFF");
+        }//else
+    } // onCreate
+
+} // addTextActivity
+
